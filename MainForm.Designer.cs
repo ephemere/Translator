804c4680d3c51e97cf14710624c0822fba6e9d1a
ephemere <nastenok1992@gmail.com>
2013-05-25 18:47:20 +0700

    Дополнен интерфейс.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,11 +30,20 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edtErmName = new System.Windows.Forms.TextBox();
+            this.edtUmlName = new System.Windows.Forms.TextBox();
+            this.btnOpenErm = new System.Windows.Forms.Button();
+            this.btnSaveUml = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,7 +54,7 @@
             this.справкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(465, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(528, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -59,6 +68,29 @@
             this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
             this.файлToolStripMenuItem.Text = "Файл";
             // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
             // справкаToolStripMenuItem
             // 
             this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -70,33 +102,86 @@
             // оПрограммеToolStripMenuItem
             // 
             this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
-            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
             this.оПрограммеToolStripMenuItem.Text = "О программе";
             this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
             // 
-            // открытьToolStripMenuItem
+            // label1
             // 
-            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
-            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Файл ERM-модели:";
             // 
-            // сохранитьToolStripMenuItem
+            // label2
             // 
-            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
-            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Файл UML-модели:";
             // 
-            // выходToolStripMenuItem
+            // edtErmName
             // 
-            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
-            this.выходToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.выходToolStripMenuItem.Text = "Выход";
-            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            this.edtErmName.Location = new System.Drawing.Point(125, 38);
+            this.edtErmName.Name = "edtErmName";
+            this.edtErmName.Size = new System.Drawing.Size(306, 20);
+            this.edtErmName.TabIndex = 3;
+            // 
+            // edtUmlName
+            // 
+            this.edtUmlName.Location = new System.Drawing.Point(124, 64);
+            this.edtUmlName.Name = "edtUmlName";
+            this.edtUmlName.Size = new System.Drawing.Size(306, 20);
+            this.edtUmlName.TabIndex = 4;
+            // 
+            // btnOpenErm
+            // 
+            this.btnOpenErm.Location = new System.Drawing.Point(437, 38);
+            this.btnOpenErm.Name = "btnOpenErm";
+            this.btnOpenErm.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenErm.TabIndex = 5;
+            this.btnOpenErm.Text = "Открыть";
+            this.btnOpenErm.UseVisualStyleBackColor = true;
+            this.btnOpenErm.Click += new System.EventHandler(this.btnOpenErm_Click);
+            // 
+            // btnSaveUml
+            // 
+            this.btnSaveUml.Location = new System.Drawing.Point(437, 62);
+            this.btnSaveUml.Name = "btnSaveUml";
+            this.btnSaveUml.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveUml.TabIndex = 6;
+            this.btnSaveUml.Text = "Сохранить";
+            this.btnSaveUml.UseVisualStyleBackColor = true;
+            this.btnSaveUml.Click += new System.EventHandler(this.btnSaveUml_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(210, 101);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(103, 23);
+            this.btnRun.TabIndex = 7;
+            this.btnRun.Text = "Запустить";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // MainForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
-            this.ClientSize = new System.Drawing.Size(465, 128);
+            this.ClientSize = new System.Drawing.Size(528, 138);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnSaveUml);
+            this.Controls.Add(this.btnOpenErm);
+            this.Controls.Add(this.edtUmlName);
+            this.Controls.Add(this.edtErmName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.menuStrip1;
@@ -119,6 +204,15 @@
         private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox edtErmName;
+        private System.Windows.Forms.TextBox edtUmlName;
+        private System.Windows.Forms.Button btnOpenErm;
+        private System.Windows.Forms.Button btnSaveUml;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -26,5 +26,41 @@ namespace Translator
         {
             Close();
         }
+
+        private void btnOpenErm_Click(object sender, EventArgs e)
+        {
+            String fileName = "";
+            openFileDialog1.Filter = "ERM-model file (*.ermmdsl)|*.ermmdsl|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                edtErmName.Text = fileName;
+            }
+        }
+
+        private void btnSaveUml_Click(object sender, EventArgs e)
+        {
+            String fileName = "";
+            saveFileDialog1.Filter = "UML-model file (*.uml)|*.uml|All Files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.FileName = "";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = saveFileDialog1.FileName;
+                edtUmlName.Text = fileName;
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnOpenErm_Click(sender, e);
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnSaveUml_Click(sender, e);
+        }
     }
 }
